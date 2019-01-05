6db9103d818ea407bb3ac402b9b018ad994d7c1c
Theo DUVAL <tduval@e1r13p2.42.fr>
2019-01-05 10:29:32 +0100

    leaks on this

--- libft/srcs/get_next_line.c
+++ libft/srcs/get_next_line.c
@@ -5,21 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tduval <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/13 20:56:40 by tduval            #+#    #+#             */
-/*   Updated: 2018/12/12 16:54:50 by tduval           ###   ########.fr       */
+/*   Created: 2019/01/05 10:28:04 by tduval            #+#    #+#             */
+/*   Updated: 2019/01/05 10:28:34 by tduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include "libft.h"
 
-static int	free_all(char *cpy, char *line)
+static int	free_all(char *line)
 {
-	if (cpy && *cpy)
-		free(cpy);
-	if (line && *line)
+	if (line)
 		free(line);
 	return (-1);
 }
@@ -29,13 +26,15 @@ static int	ft_check(char *cpy, char **line, char **buf)
 	char	*tmp;
 
 	if (!(*buf = ft_strdup(ft_strchr(cpy, '\n') + 1)))
-		return (free_all(cpy, *line));
+		return (free_all(*line));
 	if (!(tmp = ft_strdup(*line)))
-		return (free_all(cpy, *line));
+		return (free_all(*line));
 	free(*line);
 	if (!(*line = ft_strsub(tmp, 0, ft_strchr(tmp, '\n') - tmp)))
-		return (free_all(cpy, *line));
-	free(cpy);
+	{
+		free(tmp);
+		return (free_all(*line));
+	}
 	free(tmp);
 	return (0);
 }
@@ -43,25 +42,23 @@ static int	ft_check(char *cpy, char **line, char **buf)
 static int	ft_read(int fd, char **line, char **buf, int *byt)
 {
 	char	*tmp;
-	char	*cpy;
+	char	cpy[BUFF_SIZE + 1];
 
-	if (!(cpy = ft_strnew(BUFF_SIZE)))
-		return (free_all(cpy, *line));
 	while ((*byt = read(fd, cpy, BUFF_SIZE)))
 	{
 		if (*byt == -1)
-			return (free_all(cpy, *line));
+			return (free_all(*line));
 		cpy[*byt] = '\0';
 		if (!(tmp = ft_strdup(*line)))
-			return (free_all(cpy, *line));
+			return (free_all(*line));
 		free(*line);
 		if (!(*line = ft_strjoin(tmp, cpy)))
-			return (free_all(cpy, *line));
+			return (free_all(*line));
 		free(tmp);
 		if (ft_strchr(cpy, '\n'))
 		{
 			if ((ft_check(cpy, line, buf)))
-				return (free_all(cpy, *line));
+				return (free_all(*line));
 			break ;
 		}
 	}
@@ -70,35 +67,48 @@ static int	ft_read(int fd, char **line, char **buf, int *byt)
 
 static int	ft_splitfun(char **line, char **buf)
 {
+	char		*tmp;
+
+	free(*line);
 	if (!(*line = ft_strsub(*buf, 0, ft_strchr(*buf, '\n') \
-			- *buf)))
-		return (free_all(0, *line));
-	*buf = ft_strchr(*buf, '\n') + 1;
+					- *buf)))
+		return (free_all(*line));
+	if (!(tmp = ft_strdup(ft_strchr(*buf, '\n') + 1)))
+	{
+		free(*buf);
+		return (free_all(*line));
+	}
+	free(*buf);
+	if (!(*buf = ft_strdup(tmp)))
+	{
+		free(tmp);
+		return (free_all(*line));
+	}
+	free(tmp);
 	return (1);
 }
 
 int			get_next_line(const int fd, char **line)
 {
-	static char	*buf;
+	static char	*buf[10240];
 	int			byt;
 
-	if (!line || fd < 0 || BUFF_SIZE < 1 || !(*line = ft_strnew(0)))
+	if (fd > 10239 || !line || fd < 0 || (!(*line = ft_strnew(0))))
 		return (-1);
 	byt = 0;
-	if (buf && ft_strchr(buf, '\n'))
-		return (ft_splitfun(line, &buf));
-	if (buf)
+	if (buf[fd] && ft_strchr(buf[fd], '\n'))
+		return (ft_splitfun(line, &buf[fd]));
+	if (buf[fd])
 	{
-		if (!(*line = ft_strdup(buf)))
-		{
-			ft_strdel(line);
+		free(*line);
+		if (!(*line = ft_strdup(buf[fd])))
 			return (-1);
-		}
-		buf = 0;
+		free(buf[fd]);
+		buf[fd] = 0;
 	}
-	if (ft_read(fd, &(*line), &buf, &byt) == -1)
-		return (free_all(0, *line));
+	if (ft_read(fd, &(*line), &buf[fd], &byt) == -1)
+		return (free_all(*line));
 	if (byt < 1 && !ft_strlen(*line))
-		return (byt);
+		return (byt + (free_all(*line) + 1));
 	return (1);
 }
